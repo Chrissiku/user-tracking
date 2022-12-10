1bf4d8497d89f781a0229ead6511f0def066d27a
Chris Siku <chrissiku5@gmail.com>
2022-12-10 11:08:26 +0200

    Query for page views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,24 @@
 class Event < ApplicationRecord
   serialize :params, Hash
-  belongs_to :visitor
+  belongs_to :visitor  
+
+  def self.page_views
+    select(:path)
+      .where(method: "GET")
+      .group(:path)
+      .count
+  end
+
+  def self.unique_page_views
+    select(:path)
+      .from(
+        Event
+          .select(:path, :visitor_id)
+          .distinct
+          .where(method: "GET")
+          .group(:path, :visitor_id)
+      )
+      .group(:path)
+      .count(:path)
+  end
 end
--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -3,12 +3,19 @@ class Visitor < ApplicationRecord
 
     def self.time_on_site
         select("visitor_id, lower_bounds, upper_bounds")
-        .from(
-            Event.select(
-                "visitor_id, MIN(created_at) AS lower_bounds, MAX(created_at) AS upper_bounds"
-            ).group(:visitor_id)
-        ).pluck("visitor_id",
-            Arel.sql("EXTRACT(EPOCH FROM (upper_bounds - lower_bounds))").sort
-        )
+          .from(
+            Event
+              .select(
+                "visitor_id,
+                MIN(created_at) AS lower_bounds,
+                MAX(created_at) AS upper_bounds"
+              )
+              .group(:visitor_id)
+          )
+          .pluck(
+            "visitor_id",
+            Arel.sql("EXTRACT(EPOCH FROM (upper_bounds - lower_bounds))")
+          )
+          .sort
     end
 end
